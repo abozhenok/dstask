a398fb05bae393d22f09abe5c5c2c8fda5d46eea
abozhenok <artboz@hotmail.co.uk>
2024-01-25 21:53:29 +0000

    Code format

--- dstask/tests/test_dstask_pytest.py
+++ dstask/tests/test_dstask_pytest.py
@@ -1,6 +1,7 @@
 import os
 
 from uuid import uuid4
+from datetime import datetime
 from fakeredis import FakeRedis
 from unittest.mock import patch
 from dstask.backend import RedisBackend
@@ -13,6 +14,7 @@ def test_vanilla_with_redis(mock):
     backend = RedisBackend("localhost", 6379)
     expected_workflow_id = str(uuid4())
     expected_task_id = "12345"
+    expected_date = datetime.today()
     os.environ["WORKFLOW_ID"] = expected_workflow_id
     os.environ["TASK_ID"] = expected_task_id
 
@@ -23,6 +25,7 @@ def test_vanilla_with_redis(mock):
             "df": DataFrame(),
             "dictionary_type": {"test": 1},
             "number": 9.81,
+            "default_date": expected_date,
         }
 
     @ds_task(backend=backend)
@@ -32,12 +35,13 @@ def test_vanilla_with_redis(mock):
             assert expected_task_id in str(key)
 
     @ds_task(backend=backend)
-    def test_data_pull(str_type, df, dictionary_type, number):
+    def test_data_pull(str_type, df, dictionary_type, number, default_date):
         assert str_type == "e = mc^2"
         assert df.equals(DataFrame())
         assert list(dictionary_type.keys()) == ["test"]
         assert dictionary_type["test"] == 1
         assert number == 9.81
+        assert default_date == expected_date
 
     setup()
     test_workflow_and_task_id()
